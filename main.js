f892bbeb473ade9fb1804289d6be078578a00187
Katie Dawkins <katiemdawkins@gmail.com>
2022-01-13 13:59:25 -0600

    finshed

--- main.js
+++ main.js
@@ -38,12 +38,41 @@ const makeBarkMixture = (buyingObject) => {
         buyingObject.Mixed = false
     }
     
+    return buyingObject
 }
 let mixing = makeBarkMixture(buyingObject)
 console.log(mixing)
 
 
 // Bake the mixture
+//define function bakeCandy(1 param)
+    //check is mixed property === true >>> bake property =true 
+    //else baked=false
+const bakeCandy= (buyingObject) => {
+    if (buyingObject.Mixed === true){
+        buyingObject.Baked = true
+    } else {
+        buyingObject.Baked = false
+    }
+    
+    return buyingObject
+}
+
+let baking = bakeCandy(buyingObject)
+console.log(baking)
+
 // Break the hardened sheet into 6 pieces
+//defind breakBark(1param)
+    // if baked === true return array with 6 strings "Mint Chocolate Bark Piece"
+const breakBark = (buyingObject) => {
+    if (buyingObject.Baked === true){
+        return ["Mint Chocolate Bark Piece", "Mint Chocolate Bark Piece", "Mint Chocolate Bark Piece", "Mint Chocolate Bark Piece", "Mint Chocolate Bark Piece", "Mint Chocolate Bark Piece"]
+    }
+    return buyingObject
+}
+
+let finished = breakBark(buyingObject)
+console.log(finished)
+
 // 🍫 🍫 🍫 🍫 🍫 🍫 Enjoy!
 
